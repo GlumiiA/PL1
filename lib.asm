29964a4bfa4b76b9e79ad71fc17b6b0baf40dc1c
GlumiiA <168688021+GlumiiA@users.noreply.github.com>
2024-10-19 19:40:34 +0300

    Update lib.asm

--- lib.asm
+++ lib.asm
@@ -212,10 +212,13 @@ parse_uint:
         jz .end               
         sub al, '0' ; преобразование ASCII в число
         cmp al, 9  
-        ja .end ; если больше '9', выходим    
-        shl rax, 1 ; Умножаем на 2
-        add rax, rax ; Умножаем на 10
-        add rax, rbx ; добовляем новое число             
+        ja .end ; если больше '9', выходим 
+        ; Умножим текущее значение на 10
+        ; rax * 10 = (rax << 1) + (rax << 3)
+        shl rax, 1         ; Умножаем на 2
+        add rax, rax       ; Умножаем на 4
+        add rax, rax       ; Умножаем на 8
+        add rax, al        ; Теперь добавляем текущую цифру       
         inc  rdx             
         inc  rdi             
         jmp .loop_digit    
